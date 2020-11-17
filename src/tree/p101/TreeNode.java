1d97b7fb89794ca468017e0202f4a1d13bbb7e02
chenpengcode <cpcoder@aliyun.com>
2020-11-17 09:56:27 +0800

    101. 对称二叉树

---
+++ src/tree/p101/TreeNode.java
@@ -0,0 +1,18 @@
+package tree.p101;
+
+/**
+ * @Description
+ * @Author ChenPeng
+ * @Date 2020-11-16 12:14
+ */
+
+//  Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
