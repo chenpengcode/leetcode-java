66ab78a80255e44925ce7d85e9be5b78bb9833f9
chenpengcode <cpcoder@aliyun.com>
2020-11-21 03:49:15 +0800

    排序相关

---
+++ src/sort/p147/ListNode.java
@@ -0,0 +1,16 @@
+package sort.p147;
+
+/**
+ * @Description
+ * @Author ChenPeng
+ * @Date 2020-11-21 3:22
+ */
+//Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
