4ba9c39e8add7bcd179dd4260b42909addd7a4a4
chenpengcode <cpcoder@aliyun.com>
2020-11-27 01:22:10 +0800

    树相关

---
+++ src/tree/p107/TreeNode.java
@@ -0,0 +1,12 @@
+package tree.p107;
+
+//Definition for a binary tree node.
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
