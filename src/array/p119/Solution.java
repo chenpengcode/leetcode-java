f51b25c09d43444189e8259054046a394dd4f329
chenpengcode <cpcoder@aliyun.com>
2020-12-06 20:07:10 +0800

    杨辉三角

---
+++ src/array/p119/Solution.java
@@ -0,0 +1,29 @@
+package array.p119;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Description
+ * @Author CP
+ * @Date 2020/12/6
+ */
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        Integer[] dp = new Integer[rowIndex + 1];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < rowIndex + 1; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return Arrays.asList(dp);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        Solution solution = new Solution();
+        System.out.println(solution.getRow(n));
+    }
+}
