7f729cde7b31fc39d9c2f1956f6d8cf9301260ac
chenpengcode <cpcoder@aliyun.com>
2021-04-24 22:00:03 +0800

    377. 组合总和 Ⅳ

---
+++ src/dp/p377/Solution.java
@@ -0,0 +1,21 @@
+package dp.p377;
+
+/**
+ * @Description
+ * @Author CP
+ * @Date 2021/4/24
+ */
+public class Solution {
+    public int combinationSum4 (int[] nums, int target) {
+        int [] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num: nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+}
---
+++ src/dp/p377/Solution2.java
@@ -0,0 +1,31 @@
+package dp.p377;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author CP
+ * @Date 2021/4/24
+ */
+public class Solution2 {
+    Map<Integer, Integer> map = new HashMap<>();
+    public int combinationSum4(int[] nums, int target) {
+        return backtrack(nums, target);
+    }
+
+    private int backtrack(int[] nums, int remains) {
+        if (remains == 0) return 0;
+
+        if (map.containsKey(remains)) return map.get(remains);
+
+        int res = 0;
+        for (int num : nums) {
+            if (remains >= num) {
+                res += backtrack(nums, remains - num);
+            }
+        }
+        map.put(remains, res);
+        return res;
+    }
+}
