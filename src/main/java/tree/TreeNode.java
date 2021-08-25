696d47d34ea0fd2d4df612e20af15b9ffe4477e2
chenpengcode <cpcoder@aliyun.com>
2021-08-25 20:45:30 +0800

    二叉树定义

---
+++ src/main/java/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package tree;
+
+/**
+ * @Description
+ * @Author CHENPENG
+ * @Date 2021/8/25
+ */
+//Definition for a binary main.java.easy.tree node.
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
