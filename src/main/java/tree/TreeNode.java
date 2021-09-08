32c4029ec486e4dcac191ef2322d12f7f2e3ad09
chenpengcode <cpcoder@aliyun.com>
2021-09-08 22:53:11 +0800

    TreeNode

--- src/main/java/tree/TreeNode.java
+++ src/main/java/tree/TreeNode.java
@@ -1,14 +1,19 @@
 package tree;
 
 /**
- * @Description
+ * @Description Definition for a binary main.java.tree node.
  * @Author CHENPENG
  * @Date 2021/8/25
  */
-//Definition for a binary main.java.tree node.
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x) { val = x; }
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
