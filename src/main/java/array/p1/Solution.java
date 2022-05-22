f409627303c58e8f89513b263f0ebb82b5ef79aa
chenpeng <chenpeng210@outlook.com>
2022-05-23 00:41:03 +0800

    p1：两数之和

--- src/main/java/array/p1/Solution.java
+++ src/main/java/array/p1/Solution.java
@@ -1,21 +1,21 @@
-package easy.array.p1;
+package array.p1;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description 使用一个map来存储数组中的值与下标
- * @Author CHENPENG
- * @Date 2021/8/18
+ * @Description 两数之和
+ * @Author CP
+ * @Date 2020/11/28
  */
-public class Solution2 {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
-                ans[0] = i;
-                ans[1] = map.get(target - nums[i]);
+                ans[1] = i;
+                ans[0] = map.get(target - nums[i]);
             }
             map.put(nums[i], i);
         }
--- src/main/java/array/p1/Solution2.java
+++ src/main/java/array/p1/Solution2.java
@@ -1,18 +1,18 @@
-package easy.array.p1;
+package array.p1;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Description
- * @Author CP
- * @Date 2020/11/28
+ * @Description 使用一个map来存储数组中的值与下标
+ * @Author CHENPENG
+ * @Date 2021/8/18
  */
-class Solution {
+public class Solution2 {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
+        for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 ans[0] = i;
                 ans[1] = map.get(target - nums[i]);
